8b57bf80f61ef7606cf8281d54d64c0f59cef665
OSRDOM\cfir.bitan <cfir.bitan@osrenterprises.com>
2019-02-25 18:29:25 +0200

    changed http service and started scroll loading

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,21 +10,25 @@ import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 export class HomeComponent implements OnInit {
 
   data: any;
+  loadedData: any;
   modelWindow: NgbModalRef;
   opendImage: any;
+
   constructor(private httpService: HttpService, private modalService: NgbModal) { }
 
   ngOnInit() {
-    this.getNextImages(20, 20);
+    this.getImages();
+    window.addEventListener('scroll', this.scroll, true);
   }
 
-  getNextImages(count, offset) {
-    this.httpService.getimages(count, offset).subscribe(data => {
+  getImages() {
+    this.httpService.getimages().subscribe(data => {
         // @ts-ignore
         if (data.status === 'success') {
           // @ts-ignore
           this.data = data.data.result.items;
           console.log('this.data :::', this.data);
+          this.getNextImages(0, 10);
         }
       },
       err => {
@@ -32,9 +36,31 @@ export class HomeComponent implements OnInit {
       });
   }
 
+  getNextImages(count, offset) {
+    if (!this.loadedData) {
+      this.loadedData = [];
+      count = 0;
+      offset = 10;
+    }
+    const addedImages = this.data.slice(count, offset);
+    this.loadedData = this.loadedData.concat(addedImages);
+    console.log(addedImages, this.loadedData);
+  }
+
   openModal(content, image) {
     this.opendImage = image;
     this.modelWindow = this.modalService.open(content, { backdrop : true, beforeDismiss: () => false });
   }
+
+  scroll = (): void => {
+    console.log('scroll', document.documentElement.clientHeight, window.pageYOffset, document.body.scrollHeight);
+    if (document.documentElement.clientHeight <= window.pageYOffset) {
+      this.getNextImages(11, 10);
+    }
+  };
+
+/*  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+  }*/
 
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -7,17 +7,14 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  private readonly headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-  getReqOptions() {
-    return { headers: this.headers, withCredentials: true };
-  }
-
-  // getimages (count: number, offset: number) {
-  //   return this.http.get('https://api.qwant.com/api/search/images?q=dogs&t=images&count=' + count + '&offset=' + offset + '&safesearch=1&locale=en_US&uiv=4', this.getReqOptions());
+  // private readonly headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  //
+  // getReqOptions() {
+  //   return { headers: this.headers, withCredentials: true };
   // }
 
-  getimages(count: number, offset: number) {
-    return this.http.get('/assets/data/data.json', this.getReqOptions());
+  // create call to server to get images.
+  getimages() {
+    return this.http.get('/assets/data/data.json');
   }
 }
